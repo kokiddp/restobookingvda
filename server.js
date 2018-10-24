6970d8ce11dd8810a0aa7e7daaf47e093b9dfb7b
Gabriele Coquillard <gabriele.coquillard@gmail.com>
2018-10-24 20:31:20 +0200

    add 404

--- server.js
+++ server.js
@@ -29,3 +29,7 @@ var server = app.listen(process.env.PORT || 8080, function () {
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+app.use(function(req, res) {
+	res.status(404).send('404 not found');
+});
