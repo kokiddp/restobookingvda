a2640b9c4870ebae56a92466261b726db66420b4
Gabriele Coquillard <gabriele.coquillard@gmail.com>
2018-10-24 20:43:05 +0200

    handle error 500

--- server.js
+++ server.js
@@ -10,16 +10,30 @@ app.use(express.static(__dirname + '/dist'));
 
 var forceSSL = function() {
 	return function (req, res, next) {
-	if (req.headers['x-forwarded-proto'] !== 'https') {
-		return res.redirect(
-			['https://', req.get('Host'), req.url].join('')
-		);
-	}
-	next();
+		if (req.headers['x-forwarded-proto'] !== 'https') {
+			return res.redirect(
+				['https://', req.get('Host'), req.url].join('')
+			);
+		}
+		next();
 	}
 }
-
 app.use(forceSSL());
+
+var error404 = function() {
+	return function(req, res) {
+		res.status(404).send('404 not found');
+	}
+}
+app.use(error404());
+
+var error500 = function() {
+	return function(err, req, res, next) {
+		console.error(err.stack);
+		res.status(500).send('500 internal server error');
+	}
+}
+app.use(error500());
 
 var server = app.listen(process.env.PORT || 8080, function () {
 	var port = server.address().port;
@@ -28,8 +42,4 @@ var server = app.listen(process.env.PORT || 8080, function () {
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-app.use(function(req, res) {
-	res.status(404).send('404 not found');
 });
