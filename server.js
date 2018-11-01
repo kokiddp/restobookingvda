34f4279c90146ec176653f05707eb592cc78f58a
Gabriele Coquillard <gabriele.coquillard@gmail.com>
2018-11-01 02:18:12 +0100

    implement all methods

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var USERS_COLLECTION = "users";
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || dbString, function (err, client) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || dbString, {useNewUrlParser: true}, function (err, client) {
 	if (err) {
 		console.log(err);
 		process.exit(1);
@@ -31,6 +31,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || dbString, function (err, 
 
 });
 
+/*
 var forceSSL = function() {
 	return function (req, res, next) {
 		if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -42,22 +43,6 @@ var forceSSL = function() {
 	}
 }
 app.use(forceSSL());
-
-/*
-var error404 = function() {
-	return function(req, res) {
-		res.status(404).send('404 not found');
-	}
-}
-app.use(error404());
-
-var error500 = function() {
-	return function(err, req, res, next) {
-		console.error(err.stack);
-		res.status(500).send('500 internal server error');
-	}
-}
-app.use(error500());
 */
 
 app.get('/', function(req, res) {
@@ -70,25 +55,70 @@ function handleError(res, reason, message, code) {
 }
 
 app.get("/api/debug/mongodb", function(req, res) {
-	res.status(200).json(process.env.MONGODB_URI || dbString);
+	res.status(200).send(process.env.MONGODB_URI || dbString);
 });
 
 app.get("/api/users", function(req, res) {
-	handleError(res, "Not implemented", "Not implemented", 501);
+	db.collection(USERS_COLLECTION).find({}).toArray(function(err, docs) {
+		if (err) {
+			handleError(res, err.message, "Failed to get users.");
+		} else {
+			res.status(200).json(docs);
+		}
+	});
 });
 
 app.post("/api/users", function(req, res) {
-	handleError(res, "Not implemented", "Not implemented", 501);
+	var newDoc = req.body;
+	newDoc.createDate = new Date();
+
+	if (!req.body.username) {
+		handleError(res, "Invalid user input", "Must provide a username.", 400);
+	} else {
+		db.collection(USERS_COLLECTION).insertOne(newDoc, function(err, doc) {
+			if (err) {
+				handleError(res, err.message, "Failed to create new user.");
+			} else {
+				res.status(201).json(doc.ops[0]);
+			}
+		});
+	}
 });
 
 app.get("/api/users/:id", function(req, res) {
-	handleError(res, "Not implemented", "Not implemented", 501);
+	db.collection(USERS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+		if (err) {
+		  handleError(res, err.message, "Failed to get user");
+		} else {
+		  res.status(200).json(doc);
+		}
+	});
 });
 
 app.put("/api/users/:id", function(req, res) {
-	handleError(res, "Not implemented", "Not implemented", 501);
+	var updatedDoc = req.body;
+	delete updatedDoc._id;
+
+	db.collection(USERS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updatedDoc, function(err, doc) {
+		if (err) {
+			handleError(res, err.message, "Failed to update user");
+		} else {
+			updatedDoc._id = req.params.id;
+			res.status(200).json(updatedDoc);
+		}
+	});
 });
 
 app.delete("/api/users/:id", function(req, res) {
-	handleError(res, "Not implemented", "Not implemented", 501);
+	db.collection(USERS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+		if (err) {
+			handleError(res, err.message, "Failed to delete user");
+		} else {
+			res.status(200).json(req.params.id);
+		}
+	});
+});
+
+app.use(function(req, res, next) {
+	res.status(404).send('Error 404: not found');
 });
